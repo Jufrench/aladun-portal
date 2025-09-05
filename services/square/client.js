14ac15595da5f871101979dff7e72f4ebb0aab1a
Jufrench <ju.french@gmail.com>
2025-09-05 18:50:02 -0500

    hotfix env variables

--- services/square/client.js
+++ services/square/client.js
@@ -2,7 +2,7 @@ import { SquareClient, SquareEnvironment } from "square";
 
 const client = new SquareClient({
   environment: SquareEnvironment.Production,
-  token: process.env.VITE_SQUARE_ACCESS_TOKEN,
+  token: process.env.SQUARE_ACCESS_TOKEN,
 });
 
 export default client;
